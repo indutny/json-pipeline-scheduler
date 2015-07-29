59fd7a73340783ecf8a38ddf19a5d4222a2d57c5
Fedor Indutny <fedor@indutny.com>
2015-07-28 23:21:17 -0700

    test: merge/branch, refactor

--- test/api-test.js
+++ test/api-test.js
@@ -9,12 +9,7 @@ var fixtures = require('./fixtures');
 assertText.options.trim = true;
 
 describe('JSON Pipeline Scheduler', function() {
-  var p;
-  beforeEach(function() {
-    p = pipeline.create();
-  });
-
-  it('should schedule and create CFG', function() {
+  fixtures.test('example from Click\'s thesis', function(p) {
     var start = p.add('start');
     var body = p.add('region');
     var exit = p.add('region');
@@ -39,38 +34,75 @@ describe('JSON Pipeline Scheduler', function() {
 
     exit.setControl(branch);
     var ret = p.add('return', mul).setControl(exit);
-
-    var s = scheduler.create(p);
-    var out = s.run();
-
-    var actual = out.render({ cfg: true, dominance: true }, 'printable');
-    assertText.equal(actual, fixtures.fn2str(function() {/*
-      pipeline {
-        b0 {
-          i1 = literal 0
-          i2 = read()
-          i4 = literal 1
-          i5 = add i2, i4
-          i7 = literal 10
-          i10 = literal 2
-          i0 = jump
-        }
-        b0 -> b1
-        b0 => b1
-        b1 {
-          i3 = ssa:phi ^b1, i1, i6
-          i6 = add i3, i5
-          i8 = le i6, i7
-          i9 = if ^b1, i8
-        }
-        b1 -> b1, b2
-        b1 => b2
-        b1 ~> b1
-        b2 {
-          i11 = mul i6, i10
-          i12 = return ^b2, i11
-        }
+  }, function() {/*
+    pipeline {
+      b0 {
+        i1 = literal 0
+        i2 = read()
+        i4 = literal 1
+        i5 = add i2, i4
+        i7 = literal 10
+        i10 = literal 2
+        i0 = jump
       }
-    */}));
-  });
+      b0 -> b1
+      b0 => b1
+      b1 {
+        i3 = ssa:phi ^b1, i1, i6
+        i6 = add i3, i5
+        i8 = le i6, i7
+        i9 = if ^b1, i8
+      }
+      b1 -> b1, b2
+      b1 => b2
+      b1 ~> b1
+      b2 {
+        i11 = mul i6, i10
+        i12 = return ^b2, i11
+      }
+    }
+  */});
+
+  fixtures.test('merge/branch', function(p) {
+    var start = p.add('start');
+    var t = p.add('literal').addLiteral(true);
+    var branch = p.add('if', t).setControl(start);
+
+    var left = p.add('region').setControl(branch);
+
+    var leftValue = p.add('literal').addLiteral('left');
+
+    var right = p.add('region').setControl(branch);
+
+    var rightValue = p.add('literal').addLiteral('right');
+
+    var merge = p.add('region').setControl(left, right);
+    var phi = p.add('ssa:phi', [ leftValue, rightValue ]).setControl(merge);
+    var ret = p.add('return', phi).setControl(merge);
+  }, function() {/*
+    pipeline {
+      b0 {
+        i2 = literal true
+        i4 = literal "left"
+        i5 = literal "right"
+        i3 = if ^b0, i2
+      }
+      b0 -> b1, b2
+      b0 => b3, b1, b2
+      b1 {
+        i1 = jump
+      }
+      b1 -> b3
+      b1 ~> b3
+      b2 {
+        i0 = jump
+      }
+      b2 -> b3
+      b2 ~> b3
+      b3 {
+        i6 = ssa:phi ^b3, i4, i5
+        i7 = return ^b3, i6
+      }
+    }
+  */});
 });
--- test/fixtures.js
+++ test/fixtures.js
@@ -1,5 +1,26 @@
 'use strict';
 
+var assertText = require('assert-text');
+var pipeline = require('json-pipeline');
+
+var scheduler = require('../');
+
+assertText.options.trim = true;
+
 exports.fn2str = function fn2str(fn) {
   return fn.toString().replace(/^function[^{]+{\/\*|\*\/}$/g, '');
+};
+
+exports.test = function test(name, body, expected) {
+  it('should pass on ' + name, function() {
+    var p = pipeline.create();
+
+    body(p);
+
+    var s = scheduler.create(p);
+    var out = s.run();
+
+    var actual = out.render({ cfg: true, dominance: true }, 'printable');
+    assertText.equal(actual, exports.fn2str(expected));
+  });
 };
